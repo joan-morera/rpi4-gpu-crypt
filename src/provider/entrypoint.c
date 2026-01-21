95b11b0f95039d85a2a00d0fcd0767cf65f2a6d8
José Antonio Moreno <joan.morera57@gmail.com>
2026-01-21 15:01:42 +0100

    fix: Register AES-256-CTR with correct dispatch table in entrypoint.c

--- src/provider/entrypoint.c
+++ src/provider/entrypoint.c
@@ -5,13 +5,15 @@
 #include <string.h>
 
 extern const OSSL_DISPATCH vc6_aes128ctr_functions[];
+extern const OSSL_DISPATCH vc6_aes256ctr_functions[];
 extern const OSSL_DISPATCH vc6_chacha20_functions[];
 
 static const OSSL_ALGORITHM vc6_ciphers[] = {
     {"AES-128-CTR", "provider=vc6", vc6_aes128ctr_functions},
-    {"AES-256-CTR", "provider=vc6", vc6_aes128ctr_functions},
+    {"AES-256-CTR", "provider=vc6", vc6_aes256ctr_functions},
     {"ChaCha20", "provider=vc6", vc6_chacha20_functions},
     {NULL, NULL, NULL}};
+
 
 static const OSSL_DISPATCH vc6_query_operation[] = {
     {OSSL_FUNC_PROVIDER_QUERY_OPERATION, (void (*)(void))NULL}, {0, NULL}};
